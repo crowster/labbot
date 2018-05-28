0005ac8caf4453dba51669e613f8a04852b81a94
Jesusgmrtz <jesus.martinez@acftechnologies.com>
2018-05-28 15:41:51 -0500

    labbot initialized

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=acflabbot-Web-Deploy.pubxml -p:Password=cJ1BsBlvxvxDYtraCNKPgZQRcRS8oRgP3wtArQE1trFxiATFHrRREQWk0ZaY
+
